f21bad23bd61f6aa68cd4a40ad67a37023abe4c7
Isaac Gemal <isaacgemal@gmail.com>
2024-09-25 14:56:56 -0400

    Add relational capabilities

--- prisma/migrations/20240925153829_book_db/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Book" (
-    "id" SERIAL NOT NULL,
-    "Title" TEXT NOT NULL,
-    "Author" TEXT NOT NULL,
-    "ISBN" TEXT NOT NULL,
-
-    CONSTRAINT "Book_pkey" PRIMARY KEY ("id")
-);
---
+++ prisma/migrations/20240925173046_/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Profile" (
+    "name" TEXT NOT NULL,
+    "gender" TEXT NOT NULL,
+    "age" TEXT NOT NULL,
+    "userID" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_userID_key" ON "Profile"("userID");
+
+-- AddForeignKey
+ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userID_fkey" FOREIGN KEY ("userID") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
